d2c5333bdd59e05569b6f4b8e6c5d18e109bc95a
Kurniawan Ramdhani <34503939+kurniawaanr@users.noreply.github.com>
2024-11-03 09:56:31 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,70 @@
+import { navbarData, seoData } from './data'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width,initial-scale=1',
+      title: navbarData.homeTitle,
+      titleTemplate: `%s - ${navbarData.homeTitle}`,
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  sitemap: {
+    strictNuxtContentPaths: true,
+  },
+
+  site: {
+    url: seoData.mySite,
+    identity: {
+      type: 'Person',
+    },
+    twitter: seoData.twitterHandle,
+  },
+
+  typescript: {
+    strict: true,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: [
+        '/',
+        '/rss.xml',
+      ],
+    },
+  },
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'dark',
+    fallback: 'light',
+  },
+
+  modules: [
+    'nuxt-icon',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    '@nuxt/content',
+    '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    '@formkit/auto-animate',
+    '@stefanobartoletti/nuxt-social-share',
+  ],
+
+  content: {
+    highlight: {
+      theme: 'dracula',
+    },
+  },
+
+  compatibilityDate: '2024-09-30',
+})
